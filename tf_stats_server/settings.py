0a936ea6940f58f34882dd3bbad3ded20c06c2a9
Dmitry_Eremeev <deremeev@juniper.net>
2019-06-27 15:53:18 +0300

    The app takes all sensitive info from env file

--- tf_stats_server/settings.py
+++ tf_stats_server/settings.py
@@ -14,9 +14,10 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 
 import os
 
+from tf_stats_server.prod import *
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
